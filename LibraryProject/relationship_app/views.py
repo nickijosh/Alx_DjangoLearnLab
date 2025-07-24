6ce5ddd8630e1176cfc4255ae47e21ef7bff9457
Nicki Josh <nicholasjoshua26@gmail.com>
2025-07-24 12:27:37 +0100

    Initial commit for LibraryProject Django app

---
+++ LibraryProject/relationship_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Book, Library
+from django.views.generic import DetailView
+
+# Function-based view to list all books
+def list_books(request):
+    books = Book.objects.all()
+    return render(request, 'list_books.html', {'books': books})
+
+# Class-based view to show library details
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'library_detail.html'
+    context_object_name = 'library'
