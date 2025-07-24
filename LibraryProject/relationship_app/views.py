5367ef86d25304eee48be52a8900756ca6572bd2
Nicki Josh <nicholasjoshua26@gmail.com>
2025-07-24 12:36:33 +0100

    updated views

--- LibraryProject/relationship_app/views.py
+++ LibraryProject/relationship_app/views.py
@@ -5,10 +5,10 @@ from django.views.generic import DetailView
 # Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'list_books.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 # Class-based view to show library details
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
+    template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
