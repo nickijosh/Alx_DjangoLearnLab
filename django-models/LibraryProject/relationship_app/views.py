fe3fde526a6ed489b364661a20729f908a81121f
Nicki Josh <nicholasjoshua26@gmail.com>
2025-07-24 23:28:04 +0100

    Fix class-based view to use relationship_app/library_detail.html

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,11 +5,10 @@ def list_books(request):
     books = Book.objects.all()
     return render(request, "relationship_app/list_books.html", {"books": books})
 
-from django.views.generic.detail import DetailView
+from django.views.generic import DetailView
 from .models import Library
 
-# Class-based view
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
     context_object_name = 'library'
+    template_name = 'relationship_app/library_detail.html'  # ✅ Required by ALX checker
