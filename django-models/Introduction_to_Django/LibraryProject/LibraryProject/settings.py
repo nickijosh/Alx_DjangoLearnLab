aad70cc8c1c0f2f510fc69b1521f2273e3c0b06f
Nicki Josh <nicholasjoshua26@gmail.com>
2025-07-24 20:51:58 +0100

    Add views and templates for books and library

--- django-models/Introduction_to_Django/LibraryProject/LibraryProject/settings.py
+++ django-models/Introduction_to_Django/LibraryProject/LibraryProject/settings.py
@@ -15,6 +15,16 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+import os
+
+TEMPLATES = [
+    {
+        ...
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        ...
+    },
+]
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
