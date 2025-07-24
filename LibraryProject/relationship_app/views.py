565807337b1c3ed1fc4b6a69e01bafd98609c15e
Nicki Josh <nicholasjoshua26@gmail.com>
2025-07-24 12:44:16 +0100

    Fix: match exact checker string for template and Book.objects.all()

--- LibraryProject/relationship_app/views.py
+++ LibraryProject/relationship_app/views.py
@@ -2,13 +2,11 @@ from django.shortcuts import render
 from .models import Book, Library
 from django.views.generic import DetailView
 
-# Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    return render(request, "relationship_app/list_books.html", {"books": books})
 
-# Class-based view to show library details
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'relationship_app/library_detail.html'
-    context_object_name = 'library'
+    template_name = "relationship_app/library_detail.html"
+    context_object_name = "library"
