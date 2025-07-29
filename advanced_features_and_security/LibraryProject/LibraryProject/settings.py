e0f8c694e519d48c6bf9352702224adf3e9c5b0f
Nicki Josh <nicholasjoshua26@gmail.com>
2025-07-29 18:58:16 +0100

    Add advanced_features_and_security project with custom user model

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -129,6 +129,7 @@ LOGIN_REDIRECT_URL = 'list_books'
 LOGOUT_REDIRECT_URL = 'login'
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
+AUTH_USER_MODEL = 'bookshelf.CustomUser'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Book
+from django.contrib import admin
+from .models import CustomUser
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
 @admin.register(Book)
@@ -7,3 +10,14 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year') # Fields to show in list view
     search_fields = ('title', 'author')  # Enables search by title or author
     list_filter = ('publication_year',)  # Adds a filter sidebar for publication year
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('date_of_birth', 'profile_photo')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
