72c1893c23841db537b9ff2602ec4dd1cefcef38
Nicki Josh <nicholasjoshua26@gmail.com>
2025-08-07 13:35:09 +0100

    Add BookListView with filtering, searching, and ordering via DRF

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics, permissions, filters
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework as django_filters
 
 from .models import Book
 from .serializers import BookSerializer
@@ -11,7 +12,11 @@ class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
-    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
+    filter_backends = [
+        django_filters.DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
+    ]
 
      # Filtering setup
     filterset_fields = ['title', 'author', 'publication_year']
