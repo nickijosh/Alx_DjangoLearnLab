a1301a3f56c78a58b8380f1db00a05c7c99b49ed
Nicki Josh <nicholasjoshua26@gmail.com>
2025-08-21 18:26:00 +0530

    Implement serializers for user registration, login, and token retrieval in accounts app

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -10,11 +10,18 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ['id', 'username', 'email', 'bio', 'profile_picture']
 
 class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'password' 'password2']
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Passwords must match."})
+        return attrs
+    
 
     def create(self, validated_data):
         user = User.objects.create_user(
