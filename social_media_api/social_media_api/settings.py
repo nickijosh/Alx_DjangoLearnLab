8e17405000ed98aae798699bd7d638c00ad7aea7
Nicki Josh <nicholasjoshua26@gmail.com>
2025-08-21 18:57:36 +0530

    Added tagging functionality, search view, and updated URL configurations

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -130,4 +130,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
+    
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 10,
+    "DEFAULT_FILTER_BACKENDS": ["rest_framework.filters.SearchFilter"],
 }
